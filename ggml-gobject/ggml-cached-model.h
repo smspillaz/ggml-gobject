b70ed5c9d2655c76c95a37d180637ba1eccbb3d8
Sam Spilsbury <smspillaz@gmail.com>
2023-07-16 15:12:19 +0300

    ggml-gobject: Add GGMLCachedModelIstream
    This is a GFileInputStream subclass that either downloads a model
    and stores it at some specified path, or loads it from that path
    if it is already available when the stream is read.

---
+++ ggml-gobject/ggml-cached-model.h
@@ -0,0 +1,33 @@
+/*
+ * ggml-gobject/ggml-cached-model.h
+ *
+ * Library code for ggml-cached-model
+ *
+ * Copyright (C) 2023 Sam Spilsbury.
+ *
+ * ggml-gobject is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * ggml-gobject is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with ggml-gobject; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <gio/gio.h>
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define GGML_TYPE_CACHED_MODEL_ISTREAM (ggml_cached_model_istream_get_type ())
+G_DECLARE_FINAL_TYPE (GGMLCachedModelIstream, ggml_cached_model_istream, GGML, CACHED_MODEL_ISTREAM, GFileInputStream)
+
+GFileInputStream * ggml_cached_model_istream_new (const char *remote_url, const char *local_path);
+
+G_END_DECLS
